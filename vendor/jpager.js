95683e5cc36b66026c56c3214d42b12d8c102564
kanomiya <kanomiya@users.noreply.github.com>
2016-05-06 23:45:55 +0900

    Impl importing text file

--- vendor/jpager.js
+++ vendor/jpager.js
@@ -1,4 +1,19 @@
 
+function pullFile(path, callback)
+{
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', path, false);
+	
+	xhr.onreadystatechange = function()
+	{
+		if (xhr.readyState != 4) return ;
+		
+		callback(xhr.response);
+	};
+	
+	xhr.send();
+}
+
 function pullPage(path, callback)
 {
 	var xhr = new XMLHttpRequest();
@@ -9,7 +24,7 @@ function pullPage(path, callback)
 	{
 		if (xhr.readyState != 4) return ;
 		
-		callback(parsePage(xhr.responseXML));
+		parsePage(xhr.responseXML, callback);
 	};
 	
 	xhr.send();
@@ -30,12 +45,58 @@ function parseMeta(doc)
 	return metas;
 }
 
-function parsePage(doc)
+function loadFileData(elm)
+{
+	var targetTags = elm.querySelectorAll('[data-load]');
+	
+	for (var i=0; i<targetTags.length; ++i)
+	{
+		var tag = targetTags[i];
+		// var path = tag.textContent != '' ? tag.textContent : tag.dataset.src != '' ? tag.dataset.src : '';
+		var path = tag.textContent != '' ? tag.textContent : '';
+		path = path.trim();
+		
+		if (path == '') continue;
+		
+		pullFile(path, function(res)
+		{
+			tag.innerHTML = res;
+			tag.dataset.src = path;
+			parseMarkupLang(tag);
+		});
+	}
+
+}
+
+function parseMarkupLang(elm)
+{
+	var lang = elm.dataset.markupLang;
+	
+	if (lang == 'markdown') elm.innerHTML = marked(elm.innerHTML);
+}
+
+function parseChildrenMarkupLang(elm)
+{
+	var targetTags = elm.querySelectorAll('[data-markup-lang]');
+	
+	for (var i=0; i<targetTags.length; ++i)
+	{
+		parseMarkupLang(targetTags[i]);
+	}
+
+}
+
+function parsePage(doc, callback)
 {
 	var data = {};
-	data['metas'] = parseMeta(doc);
-	data['body'] = doc.body;
 	
+	data['metas'] = parseMeta(doc);
 
-	return data;
+	parseChildrenMarkupLang(doc.body);
+	data['body'] = doc.body;
+
+	var elm = callback(data);
+	
+	loadFileData(elm);
+	
 }
